fcfd8047d9158b72fc07e010979569c3e10e41f5
ivanh2048 <ivan.hrabovskyi.uam@gmail.com>
2025-06-01 12:27:14 +0200

    add customer and orders

---
+++ LogSolution/Controllers/CustomController.cs
@@ -0,0 +1,41 @@
+﻿using LogSolution.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace LogSolution.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CustomerController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var customers = _context.Customers.ToList();
+            return View(customers);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Customers.Add(customer);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
+    }
+
+}
---
+++ LogSolution/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿namespace LogSolution.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string TaxId { get; set; }
+
+        public string Address { get; set; }
+
+        public string Contact { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+    }
+
+}
